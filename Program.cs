8db081785a6d95947308ffd8c72bc42da2257a02
Esraa Ahmad Ali Bedair <100854428+esraaa7mad@users.noreply.github.com>
2022-12-14 16:02:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public abstract class person{
+ public string Name;
+ public int Age;
+ public person(string name,int age){
+  Name=name;
+  Age =age;
+ }
+ public abstract void print ();
+
+}
+
+
+
+
+
+
+
+
+
+ 
+
+public class program{
+    private static void Main() {
+     Console.WriteLine("Hello, World!");
+}
+}
